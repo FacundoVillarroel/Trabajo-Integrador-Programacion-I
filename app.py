23bd16b7c17a35e15af564fa15872ce3fa6e3843
FacundoVillarroel <facu.villarroel96@gmail.com>
2025-11-04 21:01:34 +0100

    spelling correction

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def buscar_pais(extracto_dataset):
     print("BUSCAR PAIS POR NOMBRE")
     print(separador("-", 60))
 
-    busqueda = normalizar_string(input("Ingrese el nombre del pais que desea buscar: ")).lower()
+    busqueda = normalziar_string(input("Ingrese el nombre del pais que desea buscar: ")).lower()
     
     paises_encontrados = []
     
